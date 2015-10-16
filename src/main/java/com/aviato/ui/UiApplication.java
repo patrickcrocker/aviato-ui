9b714a5f6ec57acc1115385783caff077aff0db4
patrickcrocker <patrick.crocker@gmail.com>
2015-10-16 11:04:33 -0500

    Removed dependency on commons-lang3.

--- src/main/java/com/aviato/ui/UiApplication.java
+++ src/main/java/com/aviato/ui/UiApplication.java
@@ -10,7 +10,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
